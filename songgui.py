5d33df489d216e4c397ffec828c7d3a691d5ea48
Archibald Vist <archie.m.vist@gmail.com>
2017-06-08 22:43:24 -0500

    Slightly smarter shared goalhorn button handling.

--- songgui.py
+++ songgui.py
@@ -16,7 +16,7 @@ class PlayerButtons (Frame):
       # text and buttons
       ## check if text is none (i.e., it's a reserved keyword with an overridden name)
       if self.text is None:
-         self.text = "\n".join(self.pname.split(","))
+         self.text = "\n".join([x.lstrip() for x in self.pname.split(",")])
          self.reserved = False
       else:
          self.reserved = True
