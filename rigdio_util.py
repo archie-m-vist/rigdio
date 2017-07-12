814f197fe062426c40d6fea254711aefa4b89960
Archibald Vist <archie.m.vist@gmail.com>
2017-07-12 09:25:48 +0900

    apparently these were missing?

---
+++ rigdio_util.py
@@ -0,0 +1,18 @@
+# thanks to stackoverflow: http://stackoverflow.com/questions/27650712/python-time-in-format-dayshoursminutesseconds-to-seconds
+def timeToSeconds(time):
+   multi = [1,60,3600,86400]
+   try:
+      time = [float(x) for x in time.split(":")]
+      t_ret = 0
+      for i,t in enumerate(reversed(time)):
+         t_ret += multi[i] * t
+      return t_ret
+   except ValueError:
+      return None
+
+def main():
+   print(timeToSeconds("1:22"))
+   print(timeToSeconds("0:40"))
+
+if __name__ == '__main__':
+   main()
